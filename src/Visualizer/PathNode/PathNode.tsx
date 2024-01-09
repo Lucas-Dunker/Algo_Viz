d475bab57c4948b8890783757116248d0917c71a
Lucas Dunker <dunker.l@northeastern.edu>
2024-01-09 18:10:20 -0500

    Data structure for Node, styling for grid

--- src/Visualizer/PathNode/PathNode.tsx
+++ src/Visualizer/PathNode/PathNode.tsx
@@ -1,10 +1,32 @@
 import './PathNode.css'
 
-const PathNode = () => {
-    return (<>
-        <div className='Node'>
-        </div>
-    </>
+interface NodeProps {
+    col: number,
+    isFinish: boolean,
+    isStart: boolean,
+    isWall: boolean,
+    onMouseDown: (row: number, col: number) => void,
+    onMouseEnter: (row: number, col: number) => void,
+    onMouseUp: () => void,
+    row: number
+}
+
+const PathNode = ({ col, isFinish, isStart, isWall, onMouseDown, onMouseEnter, onMouseUp, row }: NodeProps) => {
+    const extraClassName = isFinish
+        ? 'node-finish'
+        : isStart
+            ? 'node-start'
+            : isWall
+                ? 'node-wall'
+                : '';
+
+    return (
+        <div
+            id={`node-${row}-${col}`}
+            className={`node ${extraClassName}`}
+            onMouseDown={() => onMouseDown(row, col)}
+            onMouseEnter={() => onMouseEnter(row, col)}
+            onMouseUp={() => onMouseUp()}></div>
     )
 }
 
